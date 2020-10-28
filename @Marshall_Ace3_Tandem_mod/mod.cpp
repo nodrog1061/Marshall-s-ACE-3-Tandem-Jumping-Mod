5004ace2cc22356ea34c8b55f867dc90f3f62663
nodrog1061 <59317189+nodrog1061@users.noreply.github.com>
2020-10-28 19:40:49 +0000

    Inital Upload

---
+++ @Marshall_Ace3_Tandem_mod/mod.cpp
@@ -0,0 +1,13 @@
+appID = 275700;
+
+dlc = "Marshall's ACE 3 Tandem Jumping Mod";
+name = "Marshall's ACE 3 Tandem Jumping Mod";
+
+picture = "SM_tandem_jumping\data\logo\logo.paa";
+logo = "SM_tandem_jumping\data\logo\logo.paa";
+logoOver = "SM_tandem_jumping\data\logo\logo_hov.paa";
+logoSmall = "SM_tandem_jumping\data\logo\logo.paa";
+logoTitle = "SM_tandem_jumping\data\logo\logo.paa";
+
+tooltipOwned = "Created by Armed Excellence Software - Lead Dev: Scott Marshall";
+author = "Created by Armed Excellence Software - Lead Dev: Scott Marshall";
